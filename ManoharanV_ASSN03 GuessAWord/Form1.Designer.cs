8a587ee31fc211c2e1701e9491db7352c61284bd
Vaishnave <Vaishnave@VAISHNAVE-PC>
2019-04-25 22:14:38 -0400

    Add project files.

---
+++ ManoharanV_ASSN03 GuessAWord/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ManoharanV_ASSN03_GuessAWord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.lblGuess = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.btnFinishRound = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.Location = new System.Drawing.Point(26, 56);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(70, 13);
+            this.lblInstruction.TabIndex = 0;
+            this.lblInstruction.Text = "Enter a word:";
+            // 
+            // txtWord
+            // 
+            this.txtWord.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWord.Location = new System.Drawing.Point(121, 49);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(100, 29);
+            this.txtWord.TabIndex = 1;
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblDisplay.Location = new System.Drawing.Point(25, 98);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(336, 227);
+            this.lblDisplay.TabIndex = 2;
+            // 
+            // lblGuess
+            // 
+            this.lblGuess.AutoSize = true;
+            this.lblGuess.Location = new System.Drawing.Point(29, 352);
+            this.lblGuess.Name = "lblGuess";
+            this.lblGuess.Size = new System.Drawing.Size(75, 13);
+            this.lblGuess.TabIndex = 3;
+            this.lblGuess.Text = "Guess a letter:";
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtGuess.Location = new System.Drawing.Point(146, 353);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(100, 20);
+            this.txtGuess.TabIndex = 4;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(269, 46);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirm.TabIndex = 5;
+            this.btnConfirm.Text = "Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Enabled = false;
+            this.btnGuess.Location = new System.Drawing.Point(286, 353);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 23);
+            this.btnGuess.TabIndex = 6;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // btnFinishRound
+            // 
+            this.btnFinishRound.Enabled = false;
+            this.btnFinishRound.Location = new System.Drawing.Point(61, 393);
+            this.btnFinishRound.Name = "btnFinishRound";
+            this.btnFinishRound.Size = new System.Drawing.Size(80, 38);
+            this.btnFinishRound.TabIndex = 7;
+            this.btnFinishRound.Text = "Finish Round";
+            this.btnFinishRound.UseVisualStyleBackColor = true;
+            this.btnFinishRound.Click += new System.EventHandler(this.btnFinishRound_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(245, 406);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(99, 25);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 479);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnFinishRound);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblGuess);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.txtWord);
+            this.Controls.Add(this.lblInstruction);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Label lblGuess;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button btnFinishRound;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ ManoharanV_ASSN03 GuessAWord/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManoharanV_ASSN03_GuessAWord
+{
+    public partial class Form1 : Form
+    {
+        string word = "";
+        string strWordGuessedSoFar = "";
+        string guesses = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtWord.Focus();
+        }
+
+        private void txtWord_TextChanged(object sender, EventArgs e)
+        {
+            btnConfirm.Focus();
+        }
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            btnConfirm.Enabled = false;
+            btnGuess.Enabled = true;
+            btnFinishRound.Enabled = true;
+
+            word = txtWord.Text;
+            txtGuess.Focus();
+            lblDisplay.Text = "This word has " + word.Count() + " letters.";
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                strWordGuessedSoFar = strWordGuessedSoFar.PadLeft(word.Length, '*');
+            }
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            string guess = txtGuess.Text;
+            guesses += guess + " , ";
+            txtGuess.Text = "";
+            txtGuess.Focus();
+            CheckLetter(guess);
+        }
+
+
+        private void CheckLetter(string guess)
+        {
+            string[] wordArray = new string[word.Count()];
+            int count = 0;
+
+
+            for (int i = 0; i < word.Count(); i++)
+            {
+                wordArray[i] = word.Substring(i, 1);
+                if (wordArray[i] == guess)
+                {
+                    count += 1;
+                    strWordGuessedSoFar = strWordGuessedSoFar.Remove(i, 1);
+                    strWordGuessedSoFar = strWordGuessedSoFar.Insert(i, guess);
+                }
+            }
+
+            lblDisplay.Text += Environment.NewLine + "The letter " + guess + " appears " + count + " times.";
+
+            if (word == strWordGuessedSoFar)
+            {
+                btnGuess.Enabled = false;
+                btnFinishRound.Focus();
+                lblDisplay.Text += Environment.NewLine + Environment.NewLine + "Winner! You have guessed all the letters. The word was:" + Environment.NewLine + word;
+            }
+        }
+
+        private void btnFinishRound_Click(object sender, EventArgs e)
+        {
+            DisplayLettersGuessed(guesses);
+        }
+
+        private void DisplayLettersGuessed(object guess)
+        {
+            MessageBox.Show("Letters Guessed: " + guesses, "Round Finished", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            lblDisplay.Text = "";
+            txtGuess.Text = "";
+            btnFinishRound.Enabled = false;
+            btnGuess.Enabled = false;
+            btnConfirm.Enabled = true;       
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }     
+}
